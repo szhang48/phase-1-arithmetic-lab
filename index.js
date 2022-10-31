d7473d9fc561878ce1fd87aeb0cb14f38d887c3f
Sandy Zhang <sandyzhang142@gmail.com>
2022-10-31 13:03:57 -0400

    commit tree cdf8d14b6a0a0db53d80d0094edda908473abddc

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+const max = Math.max(15,16,17,18,19,20);
 
-// Write your code here
